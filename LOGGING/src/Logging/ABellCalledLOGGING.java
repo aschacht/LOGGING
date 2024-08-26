5518ac05b3fdff14b9406c4e2b65a303dbf10329
Wesley <capin_g@hotmail.com>
2024-08-25 21:17:47 -0400

    sgdfhdfh

---
+++ LOGGING/src/Logging/ABellCalledLOGGING.java
@@ -0,0 +1,20 @@
+package Logging;
+
+import java.util.HashMap;
+
+public interface ABellCalledLOGGING extends AllTheBellsAnd {
+
+	
+	LOGGINGLEVEL DEFAULTLOGGINGLOGGING = LOGGINGLEVEL.LIKEBASICALLYLOGEVEREYCHANGE;
+
+	void register_output_forLogging(LOG registerOutput,
+			HashMap<String, String> file_Names_And_Folder_Structures_For_Files);
+	
+
+
+	
+
+
+	
+	
+}
---
+++ LOGGING/src/module-info.java
@@ -0,0 +1,3 @@
+module LOGGING {
+	exports Logging;
+}
